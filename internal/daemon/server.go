46c065c602d0fd97c12a701473716b35ca7f8a89
hughneale <mail@hughneale.com>
2026-02-25 14:51:18 +0000

    Check for mode for discovery api

--- internal/daemon/server.go
+++ internal/daemon/server.go
@@ -626,7 +626,10 @@ func (s *Server) apiConfigurationHandler(c *gin.Context) {
 	workflows := []string{}  // TODO: populate workflows list
 	activities := []string{} // TODO: populate activities list
 
+	baseUrl := s.Config.GetLocalServerUrl()
+
 	if s.Config.IsServer() {
+
 		services := s.Config.GetServices()
 		capabilities["temporal"] = services.HasTemporal()
 		capabilities["vault"] = services.HasVault()
@@ -634,6 +637,8 @@ func (s *Server) apiConfigurationHandler(c *gin.Context) {
 		capabilities["scheduler"] = services.HasScheduler()
 		capabilities["llm"] = services.HasLargeLanguageModel()
 		capabilities["storage"] = services.HasStorage()
+
+		baseUrl = s.Config.GetLoginServerUrl()
 	}
 
 	response := gin.H{
@@ -643,18 +648,18 @@ func (s *Server) apiConfigurationHandler(c *gin.Context) {
 		"version":     s.GetVersion(),
 
 		// Endpoints
-		"baseUrl":     s.Config.GetLocalServerUrl(),
+		"baseUrl":     baseUrl,
 		"apiBasePath": s.Config.GetApiBasePath(),
 		//"hostname":    s.Config.Environment.Hostname,
 		//"port":        s.Config.Server.Port,
 
 		// Authentication
-		"authEndpoint": s.Config.GetLocalServerUrl() + "/auth",
+		"authEndpoint": baseUrl + "/auth",
 		"authMethods":  []string{"session", "bearer"},
 
 		// Documentation
-		"docsUrl":     s.Config.GetLocalServerUrl() + "/swagger/index.html",
-		"openApiSpec": s.Config.GetLocalServerUrl() + "/swagger/doc.json",
+		"docsUrl":     baseUrl + "/swagger/index.html",
+		"openApiSpec": baseUrl + "/swagger/doc.json",
 
 		// Capabilities
 		"workflows":    workflows,
